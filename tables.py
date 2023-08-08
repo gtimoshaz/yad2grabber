cd99a6dfcf21014ef4311470d55ae4f518be1070
Georgii Timofeevskii <gtimoshaz@gmail.com>
2023-08-08 15:16:21 +0300

    ORM Tables extracted to a module

--- tables.py
+++ tables.py
@@ -2,11 +2,12 @@ import sqlalchemy
 from typing import Optional, List
 
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
-from sqlalchemy import create_engine, text, ForeignKey
-from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy import ForeignKey, LargeBinary
+
 
 class Base(DeclarativeBase):
     pass
+
 
 class Ad(Base):
     __tablename__ = "ad"
@@ -23,12 +24,16 @@ class Ad(Base):
     property_tax: Mapped[Optional[int]]
     payments_in_year: Mapped[Optional[int]]
     furniture_info: Mapped[Optional[str]]
-    images: Mapped[List["Image"]] = relationship(back_populates="ad_id", cascade="all, delete-orphan")
+    images: Mapped[List["Image"]] = relationship(back_populates="ad")
 
 
 class Image(Base):
     __tablename__ = "images"
+
     id: Mapped[str] = mapped_column(primary_key=True)
     ad_id: Mapped[str] = mapped_column(ForeignKey("ad.id"))
-    ad: Mapped["Ad"] = relationship(back_populates="id")
+    ad: Mapped["Ad"] = relationship(back_populates="images")
+    data: Mapped[bytes]
 
+
+Base.metadata.create_all()
